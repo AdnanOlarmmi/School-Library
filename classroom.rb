3c9df6f399e82e465c370fc5fbbba144496ef799
AdnanOlarmmi <olarmmi@gmail.com>
2022-12-22 15:54:09 +0100

    Fix Has many relationship between classroom and students

--- classroom.rb
+++ classroom.rb
@@ -1,10 +1,15 @@
 
 class Classroom 
 
-    attr_accessor :label
+    attr_accessor :label 
+    attr_reader :students
 
-    def initialize (label)
+    def initialize (label, students)
         @label = label
+        @students = []
     end
 
+    def add_student (student)
+        @students << student
+    end
 end
