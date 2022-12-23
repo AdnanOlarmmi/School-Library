b36fcdb6fc899d7b18370e1883ca24ead81ec07b
AdnanOlarmmi <olarmmi@gmail.com>
2022-12-23 12:40:34 +0100

    Introduce bug

--- person.rb
+++ person.rb
@@ -3,8 +3,8 @@ require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
 
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id
+  attr_accessor :name
+  attr_reader :id, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
