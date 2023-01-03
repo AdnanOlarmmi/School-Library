a1d65d623de4b160722f68b94b29a512062208de
Aminu Rabiu <arabiu033@gmail.com>
2023-01-03 11:13:25 +0100

    Add the run method functionality

--- logic.rb
+++ logic.rb
@@ -1,6 +1,36 @@
 require_relative 'app'
-require_relative 'main'
+# require_relative 'main'
 
-def logic
-    
+class Logic
+  def initialize(app)
+    @books = []
+    @people = []
+    @rentals = []
+    @app = app
+  end
+
+  def run(option)
+    case option
+    when '1'
+      if @books.empty?
+        puts 'There are no books yet'
+        return
+      end
+      @app.list_books(@books)
+    when '2'
+      if @people.empty?
+        puts 'There are no people yet'
+        return
+      end
+      @app.list_people(@people)
+    when '3'
+      @people.push(@app.create_person)
+    when '4'
+      @books.push(@app.create_book)
+    when '5'
+      @rentals.push(@app.create_rental(@books, @people))
+    when '6'
+      @app.list_rentals_for_id
+    end
+  end
 end
